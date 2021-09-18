a9317cc00d84f1367c14dfb0d63dea9140b7c698
Jarushan Naidoo <JarushanN@discovery.co.za>
2021-09-18 10:37:53 +0200

    made components and added to git

---
+++ healthy-company-fe/src/app/mood-dialog/mood-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mood-dialog',
+  templateUrl: './mood-dialog.component.html',
+  styleUrls: ['./mood-dialog.component.css']
+})
+export class MoodDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ healthy-company-fe/src/app/toc-dialog/toc-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toc-dialog',
+  templateUrl: './toc-dialog.component.html',
+  styleUrls: ['./toc-dialog.component.css']
+})
+export class TocDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
