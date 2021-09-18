1da7d59f171a0c2f96623e94832cf4a72c470db5
Jarushan Naidoo <JarushanN@discovery.co.za>
2021-09-18 18:24:44 +0200

    login page looking nice

--- healthy-company-fe/src/app/app.module.ts
+++ healthy-company-fe/src/app/app.module.ts
@@ -8,7 +8,6 @@ import {JournalPageComponent} from './journal-page/journal-page.component';
 import {LandingPageComponent} from './landing-page/landing-page.component';
 import {MoodDialogComponent} from './mood-dialog/mood-dialog.component';
 import {TocDialogComponent} from './toc-dialog/toc-dialog.component';
-import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from './app-routing/app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BsModalRef, BsModalService} from 'ngx-bootstrap/modal';
@@ -20,9 +19,12 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 import {ComponentLoaderFactory} from 'ngx-bootstrap/component-loader';
 import {PositioningService} from 'ngx-bootstrap/positioning';
 import {MatTableModule} from '@angular/material/table';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
 import {SettingsDialogComponent} from './settings-dialog/settings-dialog.component';
 import {MatSliderModule} from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -48,6 +50,9 @@ import {MatSliderModule} from '@angular/material/slider';
     MatPaginatorModule,
     MatTableModule,
     MatSliderModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [
     FormBuilder,
@@ -59,4 +64,5 @@ import {MatSliderModule} from '@angular/material/slider';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
